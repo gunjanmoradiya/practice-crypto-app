18f9701276b1681235821940718f06f2e07097d5
gunjanmoradiya <gunjanmoradiya56@gmail.com>
2023-01-24 12:09:33 +0530

    adding project

---
+++ src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { cryptoApi } from '../Services/CryptoApi';
+import { cryptoNewsApi } from '../Services/CryptoNewsApi';
+
+export default configureStore({
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(cryptoNewsApi.middleware, cryptoApi.middleware)
+});
